0f28b140c7c4a10811c74818239773461a84ce49
Mr_Mig <alexey.migutsky@gmail.com>
2014-11-03 23:39:11 +0100

    Add module deps for angular registration

--- angular.js
+++ angular.js
@@ -12,7 +12,7 @@ module.exports = function(definition){
   try{
     angular.module(result.moduleName);
   } catch(e) {
-    angular.module(result.moduleName, []);
+    angular.module(result.moduleName, result.moduleDependencies);
   }
 
   return angular.module(result.moduleName)
