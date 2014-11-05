9599655e7b590e1576bfd8233b7eade337b6f53c
Mr_Mig <alexey.migutsky@gmail.com>
2014-11-05 14:56:12 +0100

    Added upstream bidi state update

--- index.js
+++ index.js
@@ -61,9 +61,12 @@ function makeDirectiveFactory(definition, stateName) {
         }
         if (!scope.state) {
           scope.state = new ElementState();
+          // force state to be updated for the upstream link
+          scope.$apply();
         }
         if (scope.channel) {
           Channels[scope.channel].listen(scope, scope.state);
+
         }
 
         var injectedArguments = [scope, el].concat(injectedCustomServices);
